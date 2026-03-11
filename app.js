0ca1bc6a99c18f693103522585dfcca9a06f957f
Arman Ghaziaskari <ghaziaskari@gmail.com>
2026-03-11 13:32:19 +0100

    k

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get("/", (req,res)=>{
-  res.send("Hola mundo desde Azure soy Arman 🚀")
+  res.send("Hola mundo desde Azure soy Arman 🚀. It's a test🚀🚀.")
 })
 
 app.listen(PORT, ()=>{
